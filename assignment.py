72ea416908ff51764df9c28ca825a64948780fe7
Sameer Dhakal <sameerdhakal1234@gmail.com>
2025-12-24 08:25:53 +0545

    Temperature conversion.

---
+++ assignment.py
@@ -0,0 +1,4 @@
+# 1. Remove duplicate elements from a list
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = list(set(numbers))
+print(unique_numbers)
---
+++ temperature.py
@@ -0,0 +1,11 @@
+def temp(celcius):
+    fh = (celcius*(9/5)+32)
+    kel = celcius + 273.15
+
+    print(f"The farenheit is", fh)
+    print(f"The kelvin is", kel)
+    print("\n")
+
+temp(30)
+temp(37.5)
+    
